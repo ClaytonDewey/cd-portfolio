669c985bca82c368584df98ee047302536b94aa1
ClaytonDewey <clay@dryadmedia.com>
2022-08-15 17:58:55 -0500

    Create and import Contact component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import About from "./About";
 import Portfolio from "./Portfolio";
+import Contact from "./Contact";
 import Footer from "./Footer";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <main className="container">
         <About />
         <Portfolio />
+        <Contact />
       </main>
       <Footer />
     </>
---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>
+        Contact Me
+        <br />
+        <span>Get in Touch</span>
+      </h2>
+      <article className="content">
+        <a href="mailto:clay@claytondewey.com" className="btn btn__primary">
+          clay@claytondewey.com
+        </a>
+      </article>
+    </section>
+  );
+};
+
+export default Contact;
