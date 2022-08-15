bf9e24afdc9d3897ed914f0d27f56245bcaae88b
ClaytonDewey <clay@dryadmedia.com>
2022-08-15 14:48:10 -0500

    Add header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,25 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <nav>This is the nav!</nav>
+    <header className="header__index">
+      <div className="header__wrapper">
+        <div className="page-header">
+          <img
+            src="/img/profile.jpeg"
+            className="header__image-profile"
+            alt=""
+          />
+          <h1>Clayton Dewey</h1>
+          <p className="subhead">Front End Developer</p>
+          <p className="text-center">
+            <i className="fab fa-jedi-order"></i>
+          </p>
+        </div>
+
+        <a href="#about" className="scroll-down">
+          <div className="visually-hidden">About Me</div>
+        </a>
+      </div>
     </header>
   );
 };
