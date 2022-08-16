5ff19aa3fd487311fa075470321cebe7781daf93
ClaytonDewey <clay@dryadmedia.com>
2022-08-16 13:36:20 -0500

    Import nav to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,31 @@
 import React from "react";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="header__index">
-      <div className="header__wrapper">
-        <div className="page-header">
-          <img
-            src="img/profile.jpeg"
-            className="header__image-profile"
-            alt=""
-          />
-          <h1>Clayton Dewey</h1>
-          <p className="subhead">Front End Developer</p>
-          <p className="text-center">
-            <i className="fab fa-jedi-order"></i>
-          </p>
-        </div>
+    <>
+      <Nav />
+      <header className="header__index">
+        <div className="header__wrapper">
+          <div className="page-header">
+            <img
+              src="img/profile.jpeg"
+              className="header__image-profile"
+              alt=""
+            />
+            <h1>Clayton Dewey</h1>
+            <p className="subhead">Front End Developer</p>
+            <p className="text-center">
+              <i className="fab fa-jedi-order"></i>
+            </p>
+          </div>
 
-        <a href="#about" className="scroll-down">
-          <div className="visually-hidden">About Me</div>
-        </a>
-      </div>
-    </header>
+          <a href="#about" className="scroll-down">
+            <div className="visually-hidden">About Me</div>
+          </a>
+        </div>
+      </header>
+    </>
   );
 };
 
