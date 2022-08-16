59c0dea39ab7bc2fddcbe1afea808cfa5f0c7b3c
ClaytonDewey <clay@dryadmedia.com>
2022-08-15 22:08:49 -0500

    Map through projects and display

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Portfolio = () => {
+const Portfolio = ({ projects }) => {
   return (
     <section id="portfolio">
       <h2 className="section__title">
@@ -10,46 +10,45 @@ const Portfolio = () => {
       </h2>
 
       <div className="flex flex__row-wrap">
-        <div className="project">
-          <div className="project__image">
-            <a
-              href="/"
-              aria-label="External Link"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src="/content/projects/uwbookstore/bookstore-comp.png"
-                alt=""
-              />
-            </a>
-          </div>
-
-          <div className="project__content">
-            <h2 className="project__title">University Book Store</h2>
-            <h3 className="project__type">E-Commerce Site</h3>
-            <p className="project__description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
-              accusamus cum quia ratione, hic, dignissimos adipisci molestias
-              excepturi quam asperiores enim culpa rerum debitis voluptates
-              recusandae? Consequuntur illum commodi quidem.
-            </p>
-            <a
-              href="/"
-              aria-label="External Link"
-              className="external"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              View Site
-            </a>
-            <ul className="project__tools">
-              <li>HTML</li>
-              <li>CSS/Sass</li>
-              <li>JavaScript/jQuery</li>
-            </ul>
-          </div>
-        </div>
+        {projects.map((project) => {
+          const { id, title, type, image, external, tools, description } =
+            project;
+          return (
+            <div key={id} className="project">
+              <div className="project__image">
+                <a
+                  href={external}
+                  aria-label="External Link"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <img src={`/content/projects/${image}`} alt="" />
+                </a>
+              </div>
+              <div className="project__header">
+                <h2 className="project__title">{title}</h2>
+                <h3 className="project__type">{type}</h3>
+              </div>
+              <div className="project__content">
+                <p className="project__description">{description}</p>
+                <a
+                  href={external}
+                  aria-label="External Link"
+                  className="external"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  View Site
+                </a>
+                <ul className="project__tools">
+                  {tools.map((tool) => {
+                    return <li key={tool}>{tool}</li>;
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
