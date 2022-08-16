77e22d239452d6a2cf14b37c64d5f69d9fc75d98
ClaytonDewey <clay@dryadmedia.com>
2022-08-16 11:14:51 -0500

    Wrap project content for better spacing

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,36 +15,38 @@ const Portfolio = ({ projects }) => {
             project;
           return (
             <div key={id} className="project">
-              <div className="project__image">
-                <a
-                  href={external}
-                  aria-label="External Link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <img src={`/content/projects/${image}`} alt="" />
-                </a>
-              </div>
-              <div className="project__header">
-                <h2 className="project__title">{title}</h2>
-                <h3 className="project__type">{type}</h3>
-              </div>
-              <div className="project__content">
-                <p className="project__description">{description}</p>
-                <a
-                  href={external}
-                  aria-label="External Link"
-                  className="external"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  View Site
-                </a>
-                <ul className="project__tools">
-                  {tools.map((tool) => {
-                    return <li key={tool}>{tool}</li>;
-                  })}
-                </ul>
+              <div className="project__body">
+                <div className="project__image">
+                  <a
+                    href={external}
+                    aria-label="External Link"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img src={`/content/projects/${image}`} alt="" />
+                  </a>
+                </div>
+                <div className="project__header">
+                  <h2 className="project__title">{title}</h2>
+                  <h3 className="project__type">{type}</h3>
+                </div>
+                <div className="project__content">
+                  <p className="project__description">{description}</p>
+                  <a
+                    href={external}
+                    aria-label="External Link"
+                    className="external"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    View Site
+                  </a>
+                  <ul className="project__tools">
+                    {tools.map((tool) => {
+                      return <li key={tool}>{tool}</li>;
+                    })}
+                  </ul>
+                </div>
               </div>
             </div>
           );
