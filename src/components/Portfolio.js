8cd8bf72bb873ea8fe4975e0b9f4d9f73a5dcced
ClaytonDewey <clay@dryadmedia.com>
2022-08-15 20:23:25 -0500

    Setup portfolio boilerplate HTML

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,9 +2,40 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    <div>
-      <p>This is my portfolio...</p>
-    </div>
+    <section id="portfolio">
+      <h2 className="section__title">
+        Portfolio
+        <br />
+        <span>My Work</span>
+      </h2>
+
+      <div className="flex flex__row-wrap">
+        <div className="project">
+          <div className="project__image">
+            <img
+              src="/content/projects/uwbookstore/bookstore-comp.png"
+              alt=""
+            />
+          </div>
+
+          <div className="project__content">
+            <h2 className="project__title">University Book Store</h2>
+            <h3 className="project__type">E-Commerce Site</h3>
+            <p className="project__description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+              accusamus cum quia ratione, hic, dignissimos adipisci molestias
+              excepturi quam asperiores enim culpa rerum debitis voluptates
+              recusandae? Consequuntur illum commodi quidem.
+            </p>
+            <ul className="project__tools">
+              <li>HTML</li>
+              <li>CSS/Sass</li>
+              <li>JavaScript/jQuery</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
