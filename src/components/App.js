8c3fc2742a7ef1f5bb5364364a872d2129ca0b02
ClaytonDewey <clay@dryadmedia.com>
2022-08-18 16:12:29 -0500

    Update preloader

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1500);
+    setTimeout(() => setLoading(false), 6000);
   }, []);
 
   return (
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,10 +3,29 @@ import React from "react";
 const Loader = () => {
   return (
     <div id="loader-wrapper">
-      <div id="loader"></div>
+      <div className="loader__wrap">
+        <span>
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <span>
+                      <span>
+                        <span></span>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        </span>
+      </div>
+      {/* <div id="loader"></div> */}
 
-      <div className="loader-section section-left"></div>
-      <div className="loader-section section-right"></div>
+      {/* <div className="loader-section section-left"></div>
+      <div className="loader-section section-right"></div> */}
     </div>
   );
 };
