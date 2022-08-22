7bcc3440fa644cb006bb642edd3192f5b76b3f82
ClaytonDewey <clay@dryadmedia.com>
2022-08-21 20:12:54 -0500

    Add FadeIn Section animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,90 +1,95 @@
 import React from "react";
+import FadeInSection from "./FadeInSection";
 
 const About = ({ Link }) => {
   return (
-    <section id="about">
-      <h2 className="section__title">
-        About Me
-        <br />
-        <span>Clayton Dewey</span>
-      </h2>
-      <div className="content">
-        <article className="content__left-60 px-2">
-          <p>Hi. I'm Clayton</p>
+    <FadeInSection>
+      <section id="about">
+        <h2 className="section__title">
+          About Me
+          <br />
+          <span>Clayton Dewey</span>
+        </h2>
+        <div className="content">
+          <article className="content__left-60 px-2">
+            <p>Hi. I'm Clayton</p>
 
-          <p>
-            I am a front-end web developer from Madison, WI. I am the full time
-            developer and photographer for the{" "}
-            <a href="https://www.uwbookstore.com/home">University Book Store</a>
-            , where I've been for the past 18 years.
-          </p>
+            <p>
+              I am a front-end web developer from Madison, WI. I am the full
+              time developer and photographer for the{" "}
+              <a href="https://www.uwbookstore.com/home">
+                University Book Store
+              </a>
+              , where I've been for the past 18 years.
+            </p>
 
-          <p>
-            I am currently enrolled in the Front End Developer program at{" "}
-            <a href="https://madisoncollege.edu/">Madison College</a>, and I
-            will be graduating in December 2022.
-          </p>
+            <p>
+              I am currently enrolled in the Front End Developer program at{" "}
+              <a href="https://madisoncollege.edu/">Madison College</a>, and I
+              will be graduating in December 2022.
+            </p>
 
-          <p>
-            When not working, or in class, I'm spending time with my daughter,
-            listening to music, making pictures, or all of the above.
-          </p>
+            <p>
+              When not working, or in class, I'm spending time with my daughter,
+              listening to music, making pictures, or all of the above.
+            </p>
 
-          <div className="flex btn__container">
-            <Link
-              className="btn btn__primary"
-              activeClass=""
-              to="contact"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Get In Touch
-            </Link>
-            <a
-              href="./files/Resume-Clayton_Dewey.pdf"
-              target="_blank"
-              className="btn btn__primary"
-            >
-              Download My Resume
-            </a>
-          </div>
+            <div className="flex btn__container">
+              <Link
+                className="btn btn__primary"
+                activeClass=""
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Get In Touch
+              </Link>
+              <a
+                href="./files/Resume-Clayton_Dewey.pdf"
+                target="_blank"
+                className="btn btn__primary"
+              >
+                Download My Resume
+              </a>
+            </div>
+          </article>
+          <article className="content__right-40 px-2">
+            <img src="img/about.jpg" alt="" className="img-center img about" />
+          </article>
+        </div>
+
+        <article className="mt-4">
+          <ul>
+            <li>
+              <a href="https://linkedin.com/in/claytondewey" className="mr-3">
+                <i className="fab fa-linkedin-in"></i>
+                claytondewey
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/ClaytonDewey" className="mr-3">
+                <i className="fab fa-twitter"></i>
+                @ClaytonDewey
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/dryadmedia" className="mr-3">
+                <i className="fab fa-instagram"></i>
+                @DryadMedia
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/ClaytonDewey">
+                <i className="fab fa-github"></i>
+                ClaytonDewey
+              </a>
+            </li>
+          </ul>
         </article>
-        <article className="content__right-40 px-2">
-          <img src="img/about.jpg" alt="" className="img-center img about" />
-        </article>
-      </div>
-
-      <article className="mt-4">
-        <ul>
-          <li>
-            <a href="https://linkedin.com/in/claytondewey" className="mr-3">
-              <i className="fab fa-linkedin-in"></i>
-              claytondewey
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/ClaytonDewey" className="mr-3">
-              <i className="fab fa-twitter"></i>
-              @ClaytonDewey
-            </a>
-          </li>
-          <li>
-            <a href="https://instagram.com/dryadmedia" className="mr-3">
-              <i className="fab fa-instagram"></i>
-              @DryadMedia
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/ClaytonDewey">
-              <i className="fab fa-github"></i>
-              ClaytonDewey
-            </a>
-          </li>
-        </ul>
-      </article>
-    </section>
+      </section>
+    </FadeInSection>
   );
 };
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,59 +1,62 @@
 import React from "react";
+import FadeInSection from "./FadeInSection";
 
 const Portfolio = ({ projects }) => {
   return (
-    <section id="portfolio">
-      <h2 className="section__title">
-        Portfolio
-        <br />
-        <span>My Work</span>
-      </h2>
+    <FadeInSection>
+      <section id="portfolio">
+        <h2 className="section__title">
+          Portfolio
+          <br />
+          <span>My Work</span>
+        </h2>
 
-      <div className="flex flex__row-wrap">
-        {projects.map((project) => {
-          const { id, title, type, image, external, tools, description } =
-            project;
-          return (
-            <div key={id} className="project">
-              <div className="project__body">
-                <div className="project__image">
-                  <a
-                    href={external}
-                    aria-label="External Link"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    <img src={`content/projects/${image}`} alt="" />
-                  </a>
-                </div>
-                <div className="project__header">
-                  <h2 className="project__title">{title}</h2>
-                  <h3 className="project__type">{type}</h3>
-                </div>
-                <div className="project__content">
-                  <p className="project__description">{description}</p>
-                  <a
-                    href={external}
-                    aria-label="External Link"
-                    className="external"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    View Site
-                  </a>
-                  <p className="m-0">Tools used:</p>
-                  <ul className="project__tools">
-                    {tools.map((tool) => {
-                      return <li key={tool}>{tool}</li>;
-                    })}
-                  </ul>
+        <div className="flex flex__row-wrap">
+          {projects.map((project) => {
+            const { id, title, type, image, external, tools, description } =
+              project;
+            return (
+              <div key={id} className="project">
+                <div className="project__body">
+                  <div className="project__image">
+                    <a
+                      href={external}
+                      aria-label="External Link"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <img src={`content/projects/${image}`} alt="" />
+                    </a>
+                  </div>
+                  <div className="project__header">
+                    <h2 className="project__title">{title}</h2>
+                    <h3 className="project__type">{type}</h3>
+                  </div>
+                  <div className="project__content">
+                    <p className="project__description">{description}</p>
+                    <a
+                      href={external}
+                      aria-label="External Link"
+                      className="external"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      View Site
+                    </a>
+                    <p className="m-0">Tools used:</p>
+                    <ul className="project__tools">
+                      {tools.map((tool) => {
+                        return <li key={tool}>{tool}</li>;
+                      })}
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-    </section>
+            );
+          })}
+        </div>
+      </section>
+    </FadeInSection>
   );
 };
 
